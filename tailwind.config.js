3ac247468be5cfab54786ff032a2f56e38c17173
JakeR19 <jake.reeve00@gmail.com>
2023-12-19 20:19:44 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import {fontFamily} from 'tailwindcss/defaultTheme'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Inter", ...fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
